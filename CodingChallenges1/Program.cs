1b6a5470203976085decf4e5ec6a98ee6cd8584f
bdowds <bwdowds@gmail.com>
2019-03-19 16:49:17 -0500

    Added instructions to Program class

--- CodingChallenges1/Program.cs
+++ CodingChallenges1/Program.cs
@@ -7,6 +7,12 @@ namespace CodingChallenges1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //1. Open the Challenges class and implement the methods provided.
+            //2. Run the Tests by clicking the Test tab at the top of Visual Studio and run all tests
+            //3. If you pass all tests then you can push your branch back to the GitHub repository
+
+            //*Remember to make commits throughout and work on a branch*
         }
     }
 }
